f0b3b62edd39ca741958b4d229367303dc012b6d
杉岡成哉 <a1209s518n928@gmail.com>
2023-09-30 13:08:57 +0900

    Initial Commit

---
+++ For_Observation_Study/For_Observation_StudyApp.swift
@@ -0,0 +1,17 @@
+//
+//  For_Observation_StudyApp.swift
+//  For_Observation_Study
+//
+//  Created by 杉岡成哉 on 2023/09/30.
+//
+
+import SwiftUI
+
+@main
+struct For_Observation_StudyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ For_Observation_StudyUITests/For_Observation_StudyUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  For_Observation_StudyUITestsLaunchTests.swift
+//  For_Observation_StudyUITests
+//
+//  Created by 杉岡成哉 on 2023/09/30.
+//
+
+import XCTest
+
+final class For_Observation_StudyUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
